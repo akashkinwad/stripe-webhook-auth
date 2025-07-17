6038598e189d9109326ea4e7c8d7cb0768c41701
Akash Kinwad <akashkinwad@gmail.com>
2025-07-17 22:09:15 +0530

    Added login and subscriptions views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect_to subscriptions_path
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new
+    end
   end
 
   def destroy
+    reset_session
+    redirect_to login_path
   end
 end
