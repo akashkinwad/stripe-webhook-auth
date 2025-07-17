1de240b4e2a43544d5bf27afbc3fa63b31c4d09a
Akash Kinwad <akashkinwad@gmail.com>
2025-07-17 21:48:14 +0530

    Added sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
+require "digest"
+
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+    return nil unless user
+
+    hashed = Digest::SHA256.hexdigest(password + Rails.application.secret_key_base)
+    user if user.password_digest == hashed
+  end
+
+  def set_password(password)
+    self.password_digest = Digest::SHA256.hexdigest(password + Rails.application.secret_key_base)
+  end
 end
