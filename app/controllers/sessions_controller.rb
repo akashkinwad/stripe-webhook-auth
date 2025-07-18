53491c2ab0b2af7cb4c54280e5cdd17313c72bfe
Akash Kinwad <akashkinwad@gmail.com>
2025-07-18 14:14:27 +0530

    Permit whitelisted parameters only

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.authenticate(params[:email], params[:password])
+    user = User.authenticate(session_params[:email], session_params[:password])
     if user
       session[:user_id] = user.id
       redirect_to subscriptions_path
@@ -16,5 +16,11 @@ class SessionsController < ApplicationController
   def destroy
     reset_session
     redirect_to login_path
+  end
+
+  private
+
+  def session_params
+    params.permit(:email, :password)
   end
 end
